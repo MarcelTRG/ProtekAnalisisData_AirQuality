109a646c5d46d05603bf808fcd515f9f037545f2
InfiniteTG <marceltrg123@gmail.com>
2024-03-04 21:09:04 +0700

    Initial Commit

---
+++ Board/dashboard.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+# Title Page
+st.set_page_config(page_title="Air Quality Analysis of differebt stations by Marcel Adila Jufai")
+
+# Data Read
+Data_path = 'Data CVS/Aoti.csv'
+Aoti = pd.read_csv(Data_path)
+
+# Dashboard Tittle
+st.title('Air Quality in Aotizhongxiang Stations')
+
+st.write("This Dashboard was created to fullfill the criteria needed to pass Dicoding Course: Belajar Analisis Data dengan Python")
+
+st.markdown("""
+### About Me
+- **Name**: Marcel Adila Jufai
+- **Email**: marceljufai9503@gmail.com
+- **Dicoding ID**: marceljufai
+            
+### Project Overview
+This dashboard will gives you the result of my analysis of air quality data Aotizhongxu station. This Dashboard will shown the difference of pollutant concentration [NO2, SO2, O3, CO] while also shiwn you the percentage of each pollutant the period of a year
+""")
+
+# Input Section for User
+st.subheader("Input the year and month of the data you want to know")
+select_year = st.selectbox('Select The year', list(Aoti['year'].unique()))
+select_month = st.selectbox('Select The month', list(Aoti['month'].unique()))
+
+# Filtered the data as the User inputed
+data_select = Aoti[(Aoti['year'] == select_year) & (Aoti['month'] == select_month)].copy()
+
+# Displaying data statistics
+st.subheader('Data Overview')
+st.write(data_select.describe())
+st.subheader('Data Overview for Selected Period')
+st.write(data_select.head())
+
+# Shown the linechart for pollutant concentration throught out the Year and month
+st.subheader('Line chart for pollutant concentration throughtout the year')
+
+# Filtering the data to the year that the user input
+pollutant_select = st.selectbox('Select The Pollutant', ['NO2', 'SO2', 'O3', 'CO'])
+Aoti_year = Aoti[Aoti['year'] == select_year]
+st.write(f'Data Result for {select_year}')
+st.write(Aoti_year)
+
+Pollutant_level = Aoti_year.groupby('month')[pollutant_select].mean()
+#Pollutant_level = Aoti_year.groupby('month')['O3'].mean()
+st.write(f'The Mean Result of the whole month in {select_year}')
+st.write(Pollutant_level)
+
+#V Visualize the data to a line chart
+plt.figure(figsize=(10, 6))
+Pollutant_level.plot(kind='line', marker='o', color='skyblue', linestyle='-')
+plt.title(f'{pollutant_select} Concentration on Aotizhongxu Station in the year {select_year}')
+plt.xlabel('Month')
+plt.ylabel(f'Mean of {pollutant_select}')
+plt.xticks(rotation=45)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.tight_layout()
+
+st.pyplot(plt)
+
+# Using the piechart to show the pollutant concentration
+st.subheader('Pollutant Percentage Throughtout The Year')
+data_column = ['SO2', 'NO2', 'CO', 'O3']
+data_select = Aoti_year[data_column]
+st.write(data_select)
+
+sum_values = data_select.sum()
+st.write(sum_values)
+
+plt.figure(figsize=(8, 8))
+plt.pie(sum_values, labels=sum_values.index, autopct='%1.1f%%', startangle=140)
+plt.title(f'Distribution of Air Pollutants in {select_year}')
+plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+st.pyplot(plt)
---
+++ readme.md
@@ -0,0 +1,68 @@
+# Air Quality Analysis Dicoding Project: Aotizhongzin Station
+
+## Project Overview
+This project was created to fullfill the criteria to pass the course "Belajar Data Analysis dengan Python" from Dicoding. It focuses on analyzing air quality data in that Aotizhongxing staion and gives me a general view of the pollutan in the course of 2013 to 2017 
+
+
+## Data Source
+The dataset used in this project was provided by Dicoding. I use the Aotizhongxing air quality data
+
+## Libraries Used
+- Streamlit
+- Pandas
+- Matplotlib
+- Seaborn
+- NumPy
+
+## How to Run the Dashboard
+
+To run the Air Quality Analysis Dashboard, follow these steps:
+
+### Setup Environment
+
+1. **Create and Activate a Python Environment**:
+   - You could use venv which is standard Python environment tool directly on VS Code:
+     ```
+     python -m venv [environment_name]
+     source [environment_name]/bin/activate  # On Windows use `[environment_name]\Scripts\activate`
+     ```
+
+2. **Install Required Packages**:
+   - The following packages are necessary for running the analysis and the dashboard:
+     ```
+     pip install pandas numpy matplotlib seaborn streamlit
+     ```
+
+     or you can do
+     ```
+     pip install -r requirements.txt
+     ```
+### Run the Streamlit App
+
+1. **Navigate to the Project Directory** where `dashboard.py` is located.
+
+2. **Run the Streamlit App**:
+    ```
+    cd Board
+    streamlit run dashboard.py
+    ```
+
+### Additional Files
+
+- The dataset used for this analysis is included in the Dashboard FOlder while the one that has been processed cpuld be found om Data CVS Folder.
+- A detailed Python notebook (`Proyek_Analisis_Data.ipynb`) containing the data analysis and visualizations is also provided.
+---
+### Side Note.
+Make sure to change [environment_name] when you create the virtual environment to whatever you want. In this project, i did create Virtual Environment named ProyekAoti. You could delete it and create your own named for the environment.
+
+Since the 'dashnoard.py' and 'requirement.txt need' to be in the same folder so it's easier to run the "pip install" command' i have moved the requirement.txt file to the Board folder 
+
+For the IPYNB File that was provided in this submission, i suggest that the teacher or the one that grade this submission to run it in Goofle Collabs, because it much easier there
+
+In the python file "dashboard.py", make sure to change the path for the data read [Line 10] to whereever you save this project in your computer
+---
+
+## About Me
+- **Name**: Marcel Adila Jufai
+- **Email Address**: marceljufai9503@gmail.com
+- **Dicoding ID**: marceljufai
